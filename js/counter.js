fa24d840ed500693e82a3e8144b347c6025519b2
shahbaz khan <khanshahbaz2795@gmail.com>
2026-07-11 16:00:09 +0530

    Create counter.js

---
+++ js/counter.js
@@ -0,0 +1,66 @@
+/* ==============================
+   COUNTER ANIMATION
+================================ */
+
+
+const counters =
+document.querySelectorAll(".counter");
+
+
+
+counters.forEach(counter=>{
+
+
+    counter.innerText="0";
+
+
+    const updateCounter=()=>{
+
+
+        const target =
+        +counter.dataset.target;
+
+
+        const current =
+        +counter.innerText;
+
+
+
+        const increment =
+        target / 100;
+
+
+
+        if(current < target){
+
+
+            counter.innerText =
+            Math.ceil(
+                current + increment
+            );
+
+
+            setTimeout(
+                updateCounter,
+                20
+            );
+
+
+        }
+        else{
+
+
+            counter.innerText =
+            target + "+";
+
+
+        }
+
+
+    };
+
+
+    updateCounter();
+
+
+});
